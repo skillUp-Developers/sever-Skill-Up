a9147fde8e0fb6855c2d5240ccc10f4769aa5d69
Abdikarim-dev <cabdikariim405@gmail.com>
2024-10-13 09:43:35 +0300

    created routing setup for projects...

--- src/routes/projects/Project.ts
+++ src/routes/projects/Project.ts
@@ -6,10 +6,18 @@ router.get(
   "/get-projects"
   // getProjects
 );
+router.get(
+  "/get-project/:id"
+  // getProject
+);
 
 router.post(
   "/add-project"
   // addProject
+);
+router.post(
+  "/add-projects"
+  // addProjects
 );
 
 router.post(
@@ -17,9 +25,18 @@ router.post(
   // editProject
 );
 
+router.post(
+  "/edit-projects"
+  // editProjects
+);
+
 router.delete(
   "/remove-project"
   // removeProject
+);
+router.delete(
+  "/remove-projects"
+  // removeProjects
 );
 
 export default router;
